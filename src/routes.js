450a2f1ff177528546b99638629cd1b4ae472a06
HubertKuch <kuchhubert@gmail.com>
2023-03-30 18:03:08 +0200

    Skills and technologies page

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,36 @@
 import HomeView from "./views/HomeView";
 import AboutMeView from "./views/AboutMeView";
 import ProjectsView from "./views/ProjectsView";
+import SkillsView from "./views/SkillsView";
 
 
 /** @type {[{path: string, element: JSX.Element},{path: string, element: JSX.Element},{path: string, element: JSX.Element}]} */
 const routes = [
-    {path: "/", element: <HomeView/>, exactly: true},
-    {path: "/about-me", element: <AboutMeView/>, exactly: true},
-    {path: "/projects", element: <ProjectsView/>, exactly: true},
-    {path: "*", element: <div>404</div>, exactly: true}
+    {
+        path: "/",
+        element: <HomeView/>,
+        exactly: true
+    },
+    {
+        path: "/about-me",
+        element: <AboutMeView/>,
+        exactly: true
+    },
+    {
+        path: "/projects",
+        element: <ProjectsView/>,
+        exactly: true
+    },
+    {
+        path: "/skills-and-technologies",
+        element: <SkillsView/>,
+        exactly: true
+    },
+    {
+        path: "*",
+        element: <div>404</div>,
+        exactly: true
+    }
 ];
 
 export default routes;
---
+++ src/views/SkillsView.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PageLayout from "../layouts/PageLayout";
+import ColorizedText from "../components/ColorizedText";
+
+import alpine from "../assets/svg/technologies/alpine.svg"
+import css from "../assets/svg/technologies/css.svg"
+import html from "../assets/svg/technologies/html.svg"
+import java from "../assets/svg/technologies/java.svg"
+import js from "../assets/svg/technologies/js.svg"
+import mongodb from "../assets/svg/technologies/mongodb.svg"
+import mysql from "../assets/svg/technologies/mysql.svg"
+import php from "../assets/svg/technologies/php.svg"
+import picocss from "../assets/svg/technologies/picocss.svg"
+import postgresql from "../assets/svg/technologies/postgresql.svg"
+import react from "../assets/svg/technologies/react.svg"
+import tailwindcss from "../assets/svg/technologies/tailwindcss.svg"
+import typescript from "../assets/svg/technologies/typescript.svg"
+
+function SkillsView() {
+    return (<PageLayout title={<ColorizedText content={"Skills"}/>}>
+        <div>
+            Currently I use Java, PHP, MySQL, PostgreSQL, MongoDB, JavaScript with TypeScript, React, Alpine.js,
+            PicoCSS, TailwindCSS, HTML and clean CSS.
+            <br/>
+            <br/>
+            I know a lot of design pattern, apply DDD for my bigger projects, I use e2e and unit tests using JUnit
+            (Jupiter), Mockito and PHPUnit.
+            <br/>
+            <br/>
+            In past I used to use C++ and NodeJS with ExpressJS but I don’t like those technologies.
+        </div>
+        <br/>
+        <div className={"technologies-icons"}>
+            <img src={alpine} alt=""/>
+            <img src={css} alt=""/>
+            <img src={html} alt=""/>
+            <img src={java} alt=""/>
+            <img src={js} alt=""/>
+            <img src={mongodb} alt=""/>
+            <img src={mysql} alt=""/>
+            <img src={php} alt=""/>
+            <img src={picocss} alt=""/>
+            <img src={postgresql} alt=""/>
+            <img src={react} alt=""/>
+            <img src={tailwindcss} alt=""/>
+            <img src={typescript} alt=""/>
+        </div>
+    </PageLayout>)
+}
+
+export default SkillsView;
